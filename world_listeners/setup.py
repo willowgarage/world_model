e15f87f5ab55fc92165c016e909ce811e957d8d7
Russell Toris <rctoris@wpi.edu>
2013-01-16 20:21:55 -0800

    robot listener added

--- world_listeners/setup.py
+++ world_listeners/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    scripts=['scripts/map_listener'],
+    scripts=['scripts/map_listener', 'scripts/robot_listener'],
 )
 
 setup(**d)
